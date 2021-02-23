11256d1c57eb113147f4410fc9751c23d23cdff9
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-02-23 16:10:07 -0700

    added questions and basic css style to change later

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,43 @@ const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
 
 
+function buildQuiz(){}
+
+function showResults(){}
+
+// display quiz right away
+buildQuiz();
+
+// on submit, show results
+submitButton.addEventListener('click', showResults);
+
+const myQuestions = [
+    {
+      question: "Who invented JavaScript?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which one of these is a JavaScript tag?",
+      answers: {
+        a: "html",
+        b: "img",
+        c: "funtcion"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which tool can you use to view console logs in Chrome?",
+      answers: {
+        a: "Angular",
+        b: "jQuery",
+        c: "RequireJS",
+        d: "Chrome DevTools"
+      },
+      correctAnswer: "d"
+    }
+  ];
