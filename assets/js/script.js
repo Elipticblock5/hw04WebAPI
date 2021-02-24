e7f5c1d9c20a53732eb70d174c5f6cc71bea3813
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-02-23 18:08:04 -0700

    working on html and js timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,54 @@
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById("results");
+const submitButton = document.getElementById("submit");
 
 var questions = [
   {
-      title: "Javascript can run in?",
-      choices: ["browser", "CPU", "low level memory", "earphones"],
-      answer: "browser"
+    title: "Javascript can run in?",
+    choices: ["browser", "CPU", "low level memory", "earphones"],
+    answer: "browser",
   },
   {
-      title: "The inventor of Javascript is?",
-      choices: ["Brad Eichen", "Bill Gates", "Tec Cruz", "Steve Jobs"],
-      answer: "Brad Eichen"
+    title: "The inventor of Javascript is?",
+    choices: ["Brad Eichen", "Bill Gates", "Tec Cruz", "Steve Jobs"],
+    answer: "Brad Eichen",
   },
   {
-      title: "A goot tool for debugging javascipt is:",
-      choices: ["Javascript", "terminal / bash", "for loops", "console log"],
-      answer: "console log"
+    title: "A goot tool for debugging javascipt is:",
+    choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+    answer: "console log",
   },
-
 ];
-
 
 var currentTime = document.querySelector("#currentTime");
 var timer = document.querySelector("#startTime");
 var questionsDiv = document.querySelector("#quizQuestions");
 var wrapper = document.querySelector("#wrapper");
 
-
 var secondsLeft = 60;
 
 var holdInterval = 0;
-// Holds penalty time
+// holds penalty time
 var penalty = 10;
-// Creates new element
+// creates new element
 var ulCreate = document.createElement("ul");
-
 
 // show useer timer display
 timer.addEventListener("click", function () {
+  // checking zero because its originally set to zero
 
-    // checking zero because its originally set to zero
-    
-    if (holdInterval === 0) {
-        holdInterval = setInterval(function () {
-            secondsLeft--;
-            currentTime.textContent = "Time: " + secondsLeft;
+  if (holdInterval === 0) {
+    holdInterval = setInterval(function () {
+      secondsLeft--;
+      currentTime.textContent = "Time: " + secondsLeft;
 
-            if (secondsLeft <= 0) {
-                clearInterval(holdInterval);
-                allDone();
-                currentTime.textContent = "Time's up!";
-            }
-        }, 1000);
-    }
-    render(questionIndex);
+      if (secondsLeft <= 0) {
+        clearInterval(holdInterval);
+        allDone();
+        currentTime.textContent = "Time's up!";
+      }
+    }, 1000);
+  }
+  render(questionIndex);
 });
-
-
 
