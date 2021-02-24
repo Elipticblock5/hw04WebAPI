b5223224e9b6c58aa2f14d708227b5d71756137d
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-02-23 19:44:59 -0700

    working timer need on click penalty and styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
+// Initial working timer, need to add onclick, penalty and sytle. 
 
+var sec = 45;
+var time = setInterval(myTimer, 1000);
 
-
+function myTimer() {
+    document.getElementById('timer').innerHTML = sec + "sec left";
+    sec--;
+    if (sec == -1) {
+        clearInterval(time);
+        alert("Time out!! :(");
+    }
+}
 
 
 
