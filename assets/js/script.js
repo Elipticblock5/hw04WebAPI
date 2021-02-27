6c57f92c8d0635e0b713550286a243c4e79ef6a2
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-02-27 14:03:27 -0700

    not working, starting over

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,4 @@
 
-var timerEl = document.getElementById('countdown');
-var mainEl = document.getElementById('main');
-var startBtn = document.getElementById('startBtn');
-var timeLeft = 75;
-var timerID;
-var nextBtn = document.getElementById("nextBtn");
-var queContainerEl = document.getElementById("questContainer");
-var startContainerEl = document.getElementById("startContainer");
-var questionEl = document.getElementById("question");
-var answerButtonsEl = document.getElementById("answerButtons");
-var checkAnswerEl = document.getElementById("checkAnswer");
-var viewHighScores = document.getElementById("highscores-link");
-var submitButton = document.getElementById("submitBtn");
-var clearScoreButton = document.getElementById("clearBtn");
-var initialsField = document.getElementById("playerName");
-var restartButton = document.getElementById("restartBtn");
-var scoreField = document.getElementById("playerScore");
-var scores = JSON.parse(localStorage.getItem("scores")) || [];
-
-
-
-
-
-
-
-
-
-//add build quiz function
-
-//add show results function
-
-
-
-
-
-var message =
-  'You are out of time';
-var words = message.split(' ');
-
-// add event listener for start button
-
-document.getElementById("startBtn").addEventListener("click", function() {
-  var quiz =document.getElementById("questions");
-});
-
-// worked with tutor Sierra Chapman on hide element problem.
-
-document.getElementById("questContainer").style.display = "none";
-document.getElementById("highScorecontainer").style.display = "none";
 
 
 
@@ -58,72 +9,15 @@ document.getElementById("highScorecontainer").style.display = "none";
 
 
 
-// starting question array 4 questions
-var questions = 
-[
-  {
-    question: "What is the capital of United Kingdom?",
-    choices: ["Manchester", "Birmingham", "London", "Birmingham"],
-    answer: 2
-  },
-  
-  {
-    question: "What is the capital of United States?",
-    choices: ["California", "WashingtonDC", "Miami", "Florida"],
-    answer: 1
-  }
-
-
-  
-];
 
 
 
 
 
-// Timer that counts down from 75  Lesson 4 solved problems.
-function countdown() {
-  var timeLeft = 75;
- // startn button redo from lesson 4 solved examples
-  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-  var timeInterval = setInterval(function() {
-    // As long as the `timeLeft` is greater than 1
-    if (timeLeft > 1) {
-      // Set the `textContent` of `timerEl` to show the remaining seconds
-      timerEl.textContent = timeLeft + ' seconds remaining';
-      // Decrement `timeLeft` by 1
-      timeLeft--;
-    } else if (timeLeft === 1) {
-      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-      timerEl.textContent = timeLeft + ' second remaining';
-      timeLeft--;
-    } else {
-      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-      timerEl.textContent = '';
-      // Use `clearInterval()` to stop the timer
-      clearInterval(timeInterval);
-      // Call the `displayMessage()` function
-      displayMessage();
-    }
-  }, 1000);
-}
 
-// Displays the message one word at a time
-function displayMessage() {
-  var wordCount = 0;
 
-  // Uses the `setInterval()` method to call a function to be executed every 300 milliseconds
-  var msgInterval = setInterval(function() {
-    if (words[wordCount] === undefined) {
-      clearInterval(msgInterval);
-    } else {
-      mainEl.textContent = words[wordCount];
-      wordCount++;
-    }
-  }, 300);
-}
 
-startBtn.onclick = countdown;
+
 
 
 
