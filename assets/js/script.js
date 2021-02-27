7cdcf31e6e903cfeaec08715f92daf329209fc18
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-02-27 15:00:48 -0700

    initial variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// initial variables
+var timeLeft = 75;
+var timerID
+var timerEl = document.getElementById("timer");
+var startButton = document.getElementById("start-btn");
+var nextButton = document.getElementById("next-btn");
+var startContainerEl = document.getElementById("start-box");
+var questionContainerEl = document.getElementById("question-box");
+var questionEl = document.getElementById("question");
+var answerButtonsEl = getElementById("answer-buttons");
+var checkAnswerEl = document.getElementById("check-answer");
+var viewHighScores = document.getElementById("highscores-link");
+var submitButton = document.getElementById("submit-btn");
+var clearScore = document.getElementById("clear-btn");
+var initialsBox = document.getElementById("player-name");
+var restartDoom = document.getElementById("restart-btn");
+var scoreDoom = document.getElementById("player-score");
+
 
 
 
