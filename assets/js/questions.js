3c7adb132d0cc3bd01dca75ad93278e599429bfa
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-02-27 15:46:11 -0700

    added intial functions and ? array, not working

---
+++ assets/js/questions.js
@@ -0,0 +1,39 @@
+var questions = [
+    { 
+        question: "Who invented Javascript?", 
+        answers: [
+            { text: "Bill Gates", correct: false },
+            { text: "Brad Eichen" , correct: true },
+            { text: "Al Gore", correct: false },
+            { text: "George Clooney", correct: false }
+        ]
+    },
+    { 
+        question: "Javascript is also known as?", 
+        answers: [
+            { text: "AOL", correct: false },
+            { text: "Netscape Navigator", correct: false },
+            { text: "ECMA Scrip", correct: true },
+            { text: "Microsoft Word", correct: false }
+        ]
+    },
+    { 
+        question: "How to write an IF statement for executing some code if 'i' is NOT equal to 9?", 
+        answers: [
+            { text: "if (i != 9)", correct: true },
+            { text: "if i =! 9", correct: false },
+            { text: "if (i <> 9)", correct: false },
+            { text: "if (i !=== 9)", correct: false }
+        ]
+    },
+    { 
+        question: "What is the correct way to write a JavaScript array?", 
+        answers: [
+            { text: "var fruit = (0:'orange', 1:'lemon', 2:'banana')", correct: false },
+            { text: "var fruit = ['orange', 'lemon', 'banana']", correct: true },
+            { text: "var fruit = (orange, lemon, banana)", correct: false },
+            { text: "None of the above", correct: false }
+        ]
+    },
+   
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,8 @@ var timerID
 var timerEl = document.getElementById("timer");
 var startButton = document.getElementById("start-btn");
 var nextButton = document.getElementById("next-btn");
-var startContainerEl = document.getElementById("start-box");
-var questionContainerEl = document.getElementById("question-box");
+var startBoxEl = document.getElementById("start-box");
+var questionBoxEl = document.getElementById("question-box");
 var questionEl = document.getElementById("question");
 var answerButtonsEl = getElementById("answer-buttons");
 var checkAnswerEl = document.getElementById("check-answer");
@@ -15,6 +15,115 @@ var clearScore = document.getElementById("clear-btn");
 var initialsBox = document.getElementById("player-name");
 var restartDoom = document.getElementById("restart-btn");
 var scoreDoom = document.getElementById("player-score");
+
+
+
+
+var randomQuestions, currentQuestions
+
+//start button to get first question
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestions++
+  setNextQuestion()
+});
+
+//  doom timer
+function doomClock() {
+  timeLeft--;
+  timerEl.textContent = timeLeft + " until DOOM";
+  if (timeLeft <= 0) {
+    saveScore();
+  }
+}
+
+//  doom quiz starter
+
+function doomQuiz() {
+  timerID = setInterval(timeTick, 1000);
+  startBoxEl.classList.add("hide");
+  randomQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestions = 0
+  questionBoxEl.classList.remove("hide");
+
+  timeTick();
+  setNextQuestion();
+}
+
+// next question function
+
+function setNextQuestion() {
+  resetState();
+  showQuestion(randomQuestions[currentQuestions]);
+}
+
+// dynamic to display questions
+
+function showQuestion(question) {
+  questionEl.innerText = question.question
+  question.answers.forEach(answer => {
+    var button = document.createElement("button")
+    button.innerText = answer.text
+    button.classList.add("btn")
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+
+    button.addEventListener("click", selectAnswer)
+    answerButtonsEl.appendChild(button)
+    
+  });
+};
+
+
+// reset questions
+function resetState() {
+  nextButton.classList.add("hide")
+  checkAnswerEl.classList.add("hide")
+  while (answerButtonsEl.firstChild) {
+    answerButtonsEl.removeChild
+      (answerButtonsEl.firstChild)
+  }
+};
+
+// answer selection function
+
+function selectAnswer(e) {
+  var selectedButton = e.target;
+  var correct = selectedButton.dataset.correct;
+
+  if (correct) {
+    checkAnswerEl.innerHTML = "You got it right and avoided DOOM";
+
+  } else {
+    checkAnswerEl.innerHTML = "DOOM, you lose 10 seconds";
+     if (timeLeft <= 10) {
+       timeLeft = 0;
+
+     } else {
+       timeLeft -= 10;
+     }
+  }
+
+   Array.from(answerButtonsEl.children).forEach(button => {
+     setStatusClass(button, button.dataset.correct)
+   })
+
+   if (randomQuestions.length > currentQuestions +1) {
+     nextButton.classList.remove("hide")
+     checkAnswerEl.classList.remove("hide")
+   } else {
+     startButton.classList.remove("hide")
+     saveScore();
+   }
+
+
+  
+};
+
+
+
+
 
 
 
