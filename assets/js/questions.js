1be23864d330e1c8eaebbd741030a128d2a2f16b
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-02-28 12:00:51 -0700

    changed questions, workign on high score section

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -36,21 +36,21 @@ var questions = [
         ]
     },
     { 
-        question: "What is the correct way to write a JavaScript array?", 
+        question: "Which JavaScript label catches all the values, except for the ones specified?", 
         answers: [
-            { text: "var fruit = (0:'orange', 1:'lemon', 2:'banana')", correct: false },
-            { text: "var fruit = ['orange', 'lemon', 'banana']", correct: true },
-            { text: "var fruit = (orange, lemon, banana)", correct: false },
-            { text: "None of the above", correct: false }
+            { text: "catch", correct: false },
+            { text: "label", correct: false },
+            { text: "try", correct: false },
+            { text: "default", correct: true }
         ]
     },
     { 
-        question: "What is the correct way to write a JavaScript array?", 
+        question: "JavaScrip is a _____-side programming language?", 
         answers: [
-            { text: "var fruit = (0:'orange', 1:'lemon', 2:'banana')", correct: false },
-            { text: "var fruit = ['orange', 'lemon', 'banana']", correct: true },
-            { text: "var fruit = (orange, lemon, banana)", correct: false },
-            { text: "None of the above", correct: false }
+            { text: "Client", correct: false },
+            { text: "Server", correct: false },
+            { text: "Both", correct: true },
+            { text: "None", correct: false }
         ]
     },
    
